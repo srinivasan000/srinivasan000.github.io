4517b781ec6790ba1731747d5536ebf987071e0b
srinivasan000 <srinivasans2425s@gmail.com>
2023-08-02 09:57:55 +0530

    srini

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+// toggle icon navbar
+let menu=document.querySelector("#menu");
+let navbar =document.querySelector(".nav");
+ function a(){
+    menu.classList.toggle("fa-xmark");
+    navbar.classList.toggle("active");
+}
+
+
+
+
+
+
+
+// scroll selection
+
+let sections=document.querySelectorAll('section');
+let nav=document.querySelectorAll("header .nav a");
+
+window.onscroll = () =>{
+    sections.forEach(sec =>{
+        let top=window.scrollY;
+        let offset=sec.offsetTop - 150;
+        let height=sec.offsetHeight;
+        let id=sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+nav.forEach(links=>{
+    links.classList.remove('active');
+    document.querySelector("header .nav a[href*="+ id +"]").classList.add('active');
+});
+        }
+    });
+
+
+    // sticky navbar
+    let header=document.querySelector("header");
+
+    header.classList.toggle('sticky',window.scrollY >100);
+
+    menu.classList.remove("fa-xmark");
+    navbar.classList.remove("active");
+}
+
+// type
+const typed=new Typed(".multiple-text",{
+    strings:["Web Developer","YouTuber","Full Stack Developer"],typeSpeed:100,backSpeed:100,backDelay:100,loop:true
+});
